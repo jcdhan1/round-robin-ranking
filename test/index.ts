0753203086c7be08b1348b6ee1d7327e979d1020
Jack Han <lingcheng79@googlemail.com>
2021-12-14 21:31:55 +0000

    Select teams randomly for unit tests. For #2
    Randomly selecting up to 10 teams when testing so more teams are tested.

--- test/index.ts
+++ test/index.ts
@@ -2,6 +2,33 @@ import RoundRobinRanking from '../src/index';
 import { Team } from '../src/team';
 import { expect } from 'chai';
 
+// Helpers
+const TEAMS: Array<Team> = [
+    new Team(0, 'Manchester City'),
+    new Team(1, 'Liverpool'),
+    new Team(2, 'Chelsea'),
+    new Team(3, 'West Ham United'),
+    new Team(4, 'Manchester United'),
+    new Team(5, 'Arsenal'),
+    new Team(6, 'Tottenham Hotspur'),
+    new Team(7, 'Leicester City'),
+    new Team(8, 'Wolverhampton Wanderers'),
+    new Team(9, 'Brentford')
+];
+
+/**
+ * Randomly selects n teams.
+ * 
+ * @param n how may teams to return.
+ * @returns an array of teams.
+ */
+let randomTeams = (n: number): Array<Team> => {
+    let shuffled = [...TEAMS].sort(() => 0.5 - Math.random());
+
+    return shuffled.slice(0, n);
+}
+
+// Suites
 describe('No teams', () => {
     let RRR = new RoundRobinRanking([]);
     it('should initialise with 0 teams and home-away games enabled', () => {
@@ -17,41 +44,35 @@ describe('No teams', () => {
 });
 
 describe('Teams play each other once', () => {
-    let teams: Array<Team> = [
-        new Team(1, 'Manchester United'),
-        new Team(2, 'West Ham United'),
-        new Team(3, 'Liverpool'),
-        new Team(4, 'Chelsea')
-    ];
-    let RRR = new RoundRobinRanking(teams, false);
-    it('should initialise with 4 teams with home away games disabled', () => {
-        expect(RRR.teams).to.be.an('array');
-        expect(RRR.teams).to.have.lengthOf(4);
-        expect(RRR.homeAway).to.be.false;
-    })
-    it('should schedule 6 matches', () => {
-        expect(RRR.matches).to.be.empty;
-        RRR.scheduleMatches();
-        console.log(RRR.matches);
-    })
+    for (let n = 2; n <= 10; n++) {
+        let RRR = new RoundRobinRanking(randomTeams(n), false);
+        it(`should initialise with ${n} teams with home away games disabled`, () => {
+            expect(RRR.teams).to.be.an('array');
+            expect(RRR.teams).to.have.lengthOf(n);
+            expect(RRR.homeAway).to.be.false;
+        })
+        let total: number = n * (n - 1) / 2;
+        it(`should schedule ${total} matches`, () => {
+            expect(RRR.matches).to.be.empty;
+            RRR.scheduleMatches();
+            expect(RRR.matches).to.have.lengthOf(total);
+        })
+    }
 });
 
 describe('Teams play home-away games', () => {
-    let teams: Array<Team> = [
-        new Team(1, 'Manchester United'),
-        new Team(2, 'West Ham United'),
-        new Team(3, 'Liverpool'),
-        new Team(4, 'Chelsea')
-    ];
-    let RRR = new RoundRobinRanking(teams);
-    it('should initialise with 4 teams and home-away games enabled', () => {
-        expect(RRR.teams).to.be.an('array');
-        expect(RRR.teams).to.have.lengthOf(4);
-        expect(RRR.homeAway).to.be.true;
-    })
-    it('should schedule 12 matches', () => {
-        expect(RRR.matches).to.be.empty;
-        RRR.scheduleMatches();
-        expect(RRR.matches).to.have.lengthOf(12);
-    })
+    for (let n = 2; n <= 10; n++) {
+        let RRR = new RoundRobinRanking(randomTeams(n), true);
+        it(`should initialise with ${n} teams with home away games enabled`, () => {
+            expect(RRR.teams).to.be.an('array');
+            expect(RRR.teams).to.have.lengthOf(n);
+            expect(RRR.homeAway).to.be.true;
+        })
+        let total: number = n * (n - 1);
+        it(`should schedule ${total} matches`, () => {
+            expect(RRR.matches).to.be.empty;
+            RRR.scheduleMatches();
+            expect(RRR.matches).to.have.lengthOf(total);
+        })
+    }
 });
